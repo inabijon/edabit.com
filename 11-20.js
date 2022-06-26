f9e424b3edf63c32bec8a0a9f9d4ccc698479bd2
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-06-26 09:47:49 +0500

    11-20
    joined

---
+++ 11-20.js
@@ -0,0 +1,27 @@
+//11
+
+function nextEdge(side1, side2) {
+	return (side1 + side2) - 1
+}
+nextEdge(8, 10)
+
+//12
+
+function remainder(x, y) {
+	return x % y
+}
+remainder(1, 3)
+
+//13
+
+function findPerimeter(side1, side2) {
+	return 2 * side1 + 2 * side2
+}
+findPerimeter(6, 7);
+
+//14
+
+function giveMeSomething(something) {
+	return "something" + " " + something
+}
+giveMeSomething('is better than nothing')
