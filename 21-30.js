8f4a3dc32b18e8c7ca4834d314a69f7bec002d1b
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-08-08 10:01:53 +0500

    23- 29

--- 21-30.js
+++ 21-30.js
@@ -14,3 +14,49 @@ let animals = (chickens, cows, pigs) => {
 animals(2, 3, 5)
 
 //23
+let and = (boolean1, boolean2) => {
+    if (boolean1 === true && boolean2 === true)
+        return true
+    return false
+}
+and(true, true)
+
+//24
+let isSameNum = (num1, num2) => {
+    if (num1 === num2)
+        return true
+    return false
+}
+isSameNum(2, 3)
+
+//25
+let footballPoints = (wins, draws, losses) => {
+    return wins * 3 + draws * 2 + losses * 0
+}
+footballPoints(3, 4, 2)
+
+//26 
+let convert = (hours, minutes) => {
+    return hours * 3600 + minutes * 60
+}
+convert(1, 3)
+
+//27
+let isSeven = (number) => {
+    if (number === 7)
+        return true
+    return false
+}
+isSeven(7)
+
+//28
+let checkEquality = (a, b) => a === b
+checkEquality(NaN, NaN)
+
+//29
+let profitableGamble = (prob, price, pay) => {
+    if (prob * price > pay)
+        return true
+    return false
+}
+profitableGamble(0.2, 50, 9)
